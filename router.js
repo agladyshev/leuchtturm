1940b9410e5b86dda9b06127b239269a6210a560
Alexey Gladyshev <aglad@pm.me>
2021-12-03 20:00:27 +0300

    move routing to a module

---
+++ router.js
@@ -0,0 +1,34 @@
+var location = resolvePath() || {};
+
+function getLocation() {
+    return location;
+}
+
+function resolvePath() {
+    var location = {};
+    location.pathname = document.location.pathname;
+    if (location.pathname.includes("page")) {
+        location.pageNum = location.pathname.replace(/\D/g, "");
+    }
+    return location;
+}
+
+function nextPage() {
+    if (!location.pageNum)
+        location.pageNum = 1;
+    if (location.pageNum < 100) {
+        location.pageNum++;
+        history.pushState({ id: location.pageNum }, '', `/page/${location.pageNum}`);
+    }
+}
+
+function previousPage() {
+    if (!location.pageNum)
+        location.pageNum = 1;
+    if (location.pageNum > 1) {
+        location.pageNum--;
+        history.pushState({ id: location.pageNum }, '', `/page/${location.pageNum}`);
+    }
+}
+
+export { resolvePath, nextPage, previousPage, getLocation };
