63f23bd5373502afca68a54c148b49118bac2f58
Alexey Gladyshev <aglad@pm.me>
2021-12-09 13:53:23 +0300

    add navigation between numbered pages and other views

--- NotebookApp.js
+++ NotebookApp.js
@@ -5,6 +5,40 @@ import {
 import { NotebookCover } from "./NotebookCover.js";
 import { NotebookOwner } from "./NotebookOwner.js";
 import { NotebookIndex } from "./NotebookIndex.js";
+var paths = {
+    views: ["/", "/owner", "/index", "/page/1"],
+    pagesMax: 100,
+    getNextPath() {
+        if (location.pathname.match(/^\/page\/([0-9]+)\/?$/)) {
+            let pageNum = location.pathname.replace(/\D/g, "");
+            if (pageNum == this.pagesMax)
+                return (location.pathname);
+            else
+                return (`/page/${Number(pageNum) + 1}`);
+        }
+        else {
+            let current = this.views.indexOf(location.pathname);
+            if (current + 1 == this.views.length)
+                return (location.pathname);
+            else
+                return (this.views[current + 1])
+        }
+    },
+    getPrevPath() {
+        if (location.pathname.match(/^\/page\/([0-9]+)\/?$/)) {
+            let pageNum = location.pathname.replace(/\D/g, "");
+            if (pageNum != 1) {
+                return (`/page/${Number(pageNum) - 1}`);
+            }
+        }
+        let current = this.views.indexOf(location.pathname);
+        if (current == 0)
+            return (location.pathname);
+        else
+            return (this.views[current - 1])
+
+    }
+}
 
 class NotebookApp extends HTMLElement {
     constructor() {
@@ -15,12 +49,16 @@ class NotebookApp extends HTMLElement {
         this.addEventListener("click", this.previousPageHandler);
         window.addEventListener("popstate", this.render.bind(this));
     }
-
     render() {
         if (location.pathname.match(/^\/page\/([0-9]+)\/?$/)) {
-            this.pageNum = location.pathname.replace(/\D/g, "");
-            this.createView("notebook-page");
-            this.view.render(this.pageNum);
+            let pageNum = location.pathname.replace(/\D/g, "");
+            if (pageNum > paths.pagesMax) {
+                Router.navigate("/");
+            }
+            else {
+                this.createView("notebook-page");
+                this.view.render(pageNum);
+            }
         } else if (location.pathname.match(/^\/index\/?$/)) {
             this.createView("notebook-index");
         } else if (location.pathname.match(/^\/owner\/?$/)) {
@@ -46,14 +84,18 @@ class NotebookApp extends HTMLElement {
 
     nextPageHandler(e) {
         if (e.target && e.target.id == "btn-next") {
-            Router.nextPage();
+            console.log(paths.getNextPath());
+            Router.navigate(paths.getNextPath());
+            // Router.nextPage();
             // this.render();
         }
     }
 
     previousPageHandler(e) {
         if (e.target && e.target.id == "btn-prev") {
-            Router.previousPage();
+            console.log(paths.getPrevPath());
+            Router.navigate(paths.getPrevPath());
+            // Router.previousPage();
             // this.render();
         }
     }
--- router.js
+++ router.js
@@ -1,22 +1,3 @@
-var location = resolvePath() || {};
-
-function getLocation() {
-    return location;
-}
-
-function resolvePath() {
-    var location = {};
-    location.pathname = document.location.pathname;
-    if (location.pathname.includes("page")) {
-        // location.page = "dotted";
-        location.pageNum = location.pathname.replace(/\D/g, "");
-    } else if (location.pathname == "/") {
-        // location.page = "main";
-        location.pageNum = 1;
-    }
-    return location;
-}
-
 function navigate(path) {
     // var state = { id: location.pageNum };
     var state = {};
@@ -25,41 +6,11 @@ function navigate(path) {
 
 }
 
-function nextPage() {
-    if (!location.pageNum)
-        location.pageNum = 1;
-    if (location.pageNum < 100) {
-        location.pageNum++;
-        var state = { id: location.pageNum };
-        history.pushState({
-            state
-        }, '', `/page/${location.pageNum}`);
-        sendPopState(state);
-    }
-}
-
-function previousPage() {
-    if (!location.pageNum)
-        location.pageNum = 1;
-    if (location.pageNum > 1) {
-        location.pageNum--;
-        var state = { id: location.pageNum };
-        history.pushState({
-            state
-        }, '', `/page/${location.pageNum}`);
-        sendPopState(state);
-    }
-}
-
 function sendPopState(state) {
     var popStateEvent = new PopStateEvent('popstate', { state: state });
     dispatchEvent(popStateEvent);
 }
 
 export {
-    resolvePath,
-    nextPage,
-    previousPage,
-    getLocation,
     navigate
 };
