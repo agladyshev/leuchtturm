b940bd8077f2c283f53f624cd4bc304dc8e56cc1
Alexey Gladyshev <aglad@pm.me>
2021-12-21 13:12:16 +0300

    Set layout for owner info page

--- NotebookOwner.js
+++ NotebookOwner.js
@@ -14,25 +14,40 @@ class NotebookOwner extends HTMLElement {
         var stylesheet = document.createElement('link');
         stylesheet.setAttribute('href', '/owner.css');
         stylesheet.setAttribute('rel', 'stylesheet');
-        // var asideLeft = document.createElement('aside');
-        // asideLeft.className = "aside-left";
-        // var header = document.createElement('header');
-        // var asideRight = document.createElement('aside');
-        // asideRight.className = "aside-right";
-        // this.grid = document.createElement('article');
-        // this.grid.className = "grid";
         // var footer = document.createElement('footer');
+        var header = document.createElement('header');
+        header.innerHTML = `
+        <h6>bitte zurücksenden an</h6>
+        <h6>please return to</h6>
+        <h6> veuillez s.v.p. retourner à</h6>
+        `
+        var form = document.createElement('form');
+        for (let i = 0; i < 6; i++) {
+            let line = document.createElement('input');
+            form.appendChild(line);
+        }
+        var footer = document.createElement('footer');
+        footer.innerHTML = `
+        <h6>herzlichen dank</h6>
+        <h6>thank you very much</h6>
+        <h6>merci beaucoup</h6>
+        `
         this.shadowRoot.appendChild(stylesheet);
-        // this.shadowRoot.appendChild(asideLeft);
-        // this.shadowRoot.appendChild(header);
-        // this.shadowRoot.appendChild(this.grid);
-        // this.shadowRoot.appendChild(footer);
-        // this.shadowRoot.appendChild(asideRight);
+        this.shadowRoot.appendChild(header);
+        this.shadowRoot.appendChild(form);
+        this.shadowRoot.appendChild(footer);
     }
-    addListeners() {
+    getValuesFromStorage() {
+        var owner;
+        owner = JSON.parse(localStorage.getItem("owner"));
+        if (!owner) {
+            owner = localStorage.setItem("owner", ["", "", "", "", "", ""]);
+            owner = localStorage.getItem("owner");
+        }
+        this.owner = owner;
     }
-    render() {
-    }
+    addListeners() {}
+    render() {}
 }
 
 customElements.define("notebook-owner", NotebookOwner);
